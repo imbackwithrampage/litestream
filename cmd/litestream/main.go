bbea9ce9d652e1936e00f0ced3e59c6576dc1c7c
Toni Spets <toni.spets@beeper.com>
2022-10-17 10:28:33 +0300

    Add age encryption support

--- cmd/litestream/main.go
+++ cmd/litestream/main.go
@@ -17,6 +17,7 @@ import (
 	"strings"
 	"time"
 
+	"filippo.io/age"
 	"github.com/benbjohnson/litestream"
 	"github.com/benbjohnson/litestream/abs"
 	"github.com/benbjohnson/litestream/file"
@@ -323,6 +324,10 @@ type ReplicaConfig struct {
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
 	KeyPath  string `yaml:"key-path"`
+
+	// Encryption identities and recipients
+	AgeIdentities []string `yaml:"age-identities"`
+	AgeRecipients []string `yaml:"age-recipients"`
 }
 
 // NewReplicaFromConfig instantiates a replica for a DB based on a config.
@@ -348,6 +353,22 @@ func NewReplicaFromConfig(c *ReplicaConfig, db *litestream.DB) (_ *litestream.Re
 	}
 	if v := c.ValidationInterval; v != nil {
 		r.ValidationInterval = *v
+	}
+	for _, str := range c.AgeIdentities {
+		identities, err := age.ParseIdentities(strings.NewReader(str))
+		if err != nil {
+			return nil, err
+		}
+
+		r.AgeIdentities = append(r.AgeIdentities, identities...)
+	}
+	for _, str := range c.AgeRecipients {
+		recipients, err := age.ParseRecipients(strings.NewReader(str))
+		if err != nil {
+			return nil, err
+		}
+
+		r.AgeRecipients = append(r.AgeRecipients, recipients...)
 	}
 
 	// Build and set client on replica.
